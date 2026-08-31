92ccf2bcc3a4cf392f021f890427967157b9ea15
ayushvyas-dev <ayushvyas909@gmail.com>
2026-08-31 22:08:22 +0530

    update conversionOperations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -14,3 +14,16 @@ let convertToString = String(num);
 console.log(typeof(value));
 console.log(typeof(num));
 console.log(typeof(convertToNumber));
+
+//string concatenation
+let str1 = "Hello";
+let str2 = "World";
+let str3 = str1 + " " + str2;
+console.log(str3);
+
+// if string is first then number is converted to string and concatenated
+// if number is first then string is converted to number and added
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
