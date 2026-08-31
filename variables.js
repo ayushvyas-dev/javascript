749773f95a2f3603663a18d0c2e7e94af07fb8f0
ayushvyas-dev <ayushvyas909@gmail.com>
2026-08-31 21:22:02 +0530

    add variable notes

---
+++ variables.js
@@ -0,0 +1,20 @@
+// javascript basics
+
+// var is global scoped
+// never use var
+// let, const are block scoped
+// let can be reassigned, const cannot be reassigned
+
+const name = 'ayush';
+let age = 21;
+
+// variable with no values are undefined 
+let color;
+
+
+// to print on console
+console.log("Hello World!");
+
+// to print in table format
+console.table([name,age,color]);
+console.table({name, age})
